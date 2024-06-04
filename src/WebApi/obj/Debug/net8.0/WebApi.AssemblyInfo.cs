5d8d8b7a6f542eb8c9e6b71ace844722cf30e72c
Andrés Aguilar <andresaguilarjs@gmail.com>
2024-06-03 23:23:27 -0500

    feet: add .gitignore for .net project

--- src/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ src/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db58b3f2ff7df75f6bfc6768c435b62cf89f5552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
