2bfaa93adaed00a8449e8999ce166a81e9cf9c2b
Andrés Aguilar <andresaguilarjs@gmail.com>
2024-06-10 21:10:46 -0500

    fix: fix User Entity configuration

--- src/Infrastructure/Database/Configurations/UserConfiguration.cs
+++ src/Infrastructure/Database/Configurations/UserConfiguration.cs
@@ -1,4 +1,5 @@
 using Domain.Entities.User;
+using Domain.Entities.User.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -8,15 +9,26 @@ namespace Infrastructure.Database.Configurations
     {
         public void Configure(EntityTypeBuilder<UserEntity> builder)
         {
+            builder.ToTable("users");
+
             builder.HasKey(x => x.Id);
             builder.Property(x => x.Id).ValueGeneratedOnAdd();
 
-            builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasConversion(email => email.Value, email => new Email(email));
             builder.HasIndex(x => x.Email).IsUnique();
 
-            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasConversion(firstName => firstName.Value, firstName => new FirstName(firstName));
 
-            builder.Property(x => x.LastName).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(100)
+                .HasConversion(lastName => lastName.Value, lastName => new LastName(lastName));
 
             builder.Property(x => x.CreatedAt).IsRequired();
 
